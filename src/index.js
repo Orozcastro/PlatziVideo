d72c8498a6a268a1531ee42c281053bafffd7377
Orozcastro <47483805+Orozcastro@users.noreply.github.com>
2021-03-26 19:16:02 -0600

    ESLint

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+
+// usar reacr dom para pasarle los componentes
+ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
